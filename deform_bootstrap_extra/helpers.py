4a533d74837b7834f170da235c9b47e77bf59314
Nando Florestan <nandoflorestan@gmail.com>
2013-03-17 17:11:02 -0300

    Fix division bug in lengthen()

--- deform_bootstrap_extra/helpers.py
+++ deform_bootstrap_extra/helpers.py
@@ -1,38 +1,37 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 '''Helper functions for colander and deform.'''
 
 from __future__ import (absolute_import, division, print_function,
-    unicode_literals)
+                        unicode_literals)
 import colander as c
 import deform.widget as w
 from bag.sqlalchemy.tricks import length
 
 
 def lengthen(max, min=0, size=None, widget_cls=w.TextInputWidget,
-    typ='input', placeholder=None, validators=None):
+             typ='text', placeholder=None, validators=None):
     '''Use this to easily create well-sized inputs.
 
-    Returns a dict containing *widget* and *validator*,
-    all concerned about length.
+        Returns a dict containing *widget* and *validator*,
+        all concerned about length.
 
-    If the parameter *max* is not an integer, it is treated as a SQLALchemy
-    model property from which the real *max* can be inferred.
+        If the parameter *max* is not an integer, it is treated as a SQLALchemy
+        model property from which the real *max* can be inferred.
 
-    Example usage::
+        Example usage::
 
-        from deform_bootstrap_extra.helpers import lengthen
-        import colander as c
+            from deform_bootstrap_extra.helpers import lengthen
+            import colander as c
 
-        class ContactSchema(CSRFSchema):
-            name = c.SchemaNode(c.Str(), title=_("Name"), missing=None,
-                **lengthen(Contact.name))  # this is a model property
-    '''
+            class ContactSchema(CSRFSchema):
+                name = c.SchemaNode(c.Str(), title=_("Name"), missing=None,
+                    **lengthen(Contact.name))  # this is a model property
+        '''
     if not isinstance(max, int):
         max = length(max)
     if not size:
-        size = max if max <= 35 else 35 + (max - 35) / 4
+        size = int(max if max <= 35 else 35 + (max - 35) / 4)
     if size > 60:
         size = 60
     validator = c.Length(min=min, max=max)
